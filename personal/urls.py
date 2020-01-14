ce3af6286987d5d3eb6be72ff3d308bf0797ca31
frek <frek.mown@gmail.com>
2020-01-14 17:08:33 +0530

    first commit

---
+++ personal/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    # path('chemocobra', metabolic_views.MetabolicAppView.as_view(),name='metabolic_app'),
+    path('chemocobra/', include('chemocobra.urls')),
+    path('museo-solidaridad/', include('museo_solidaridad.urls')),
+    path('geodata/', include('geodata.urls')),
+    path('', include('main.urls')),
+]
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Chemocobra: Chemoinformatics & Metabolic Modelling
+
+To install:
+
+1. Install conda 
+2. conda env create -f deployment/chemocobra-env.yml chemocobra-env
+
+To run:
+1. conda activate chemocobra-env
+2. python manage.py runserver
+3. go to http://localhost:8000/
