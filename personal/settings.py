24076378dd79ba0f97cf1b381f1e2f300c8fbbb3
frek <frek.mown@gmail.com>
2020-01-27 16:38:54 +0530

    frekmown.space added as allowed host

--- personal/settings.py
+++ personal/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '1u^ie*&x*ze#z0@ey_mh64$9k&7o-ki2it5(_t09n-7u)3lcwc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["frekmown.space"]
 
 
 # Application definition
